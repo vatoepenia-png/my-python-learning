7b45a259c73871802e0675209e6373c212c8f71d
vatoepenia-png <vatoepenia@gmail.com>
2026-05-13 01:24:25 +0400

    Fixed variables and f-strings

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello Vato! My first code is running.")
+# Defining variables
+name = "Vato Ephenia"
+current_job = "Store Manager"
+goal = "Web Developer"
+learning_period_months = 18
+
+# Printing the output
+print(f"Hello, my name is {name}.")
+print(f"I am currently a {current_job}.")
+print(f"My goal is to become a {goal} within the next {learning_period_months} months.")
