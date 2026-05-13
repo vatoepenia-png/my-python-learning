570839ee080b8d27c9081ce345fbfa66edfb84c5
Vato Ephenia <vatoepenia@gmail.com>
2026-05-13 23:52:34 +0400

    inventory items

--- main.py
+++ main.py
@@ -15,3 +15,19 @@ age_number = int(age_text)
 print(f"Original type: {type(age_text)}")
 print(f"New type: {type(age_number)}")
 print(f"Next year I will be: {age_number + 1}")
+
+inventory = ["iphone15", "macbook air", "airpods pro"]
+
+print(f"inventory list: {inventory}")
+print(f"type of inventory: (type{inventory})")
+
+# 0 = iphone 15, 1 = macbook air, 2 = airpods pro 
+print(f"firs item in store: {inventory[0]}")
+inventory.append("apple watch")
+print(f"updated inventory: {inventory}")
+
+total_items = len(inventory)
+has_iphone_15 = True
+print(f"second item in store: {inventory[1]}")
+print(f"total numbers of items in inventory: {total_items}")
+print(f"has_iphone_15? {has_iphone_15} and its type is: {type(has_iphone_15)}")
