5a51d3c83df62f8f424625e31b09365984475193
Vato Ephenia <vatoepenia@gmail.com>
2026-05-19 22:28:45 +0400

    json princips

--- store_manager.py
+++ store_manager.py
@@ -1,51 +1,54 @@
-products = {
-    "iphone_15": 2500,
-    "macbook_air": 4000,
-    "airpods_pro": 700,
-    "apple_watch": 900
-}
+import json
+with open("my_backup.json") as file:
+    products = json.load(file)
 
 def calculate_tax(price):
     return price * 0.18
-
-def apply_discount(price, percent):
+def apple_discount(price, percent):
     return price - (price * (percent / 100))
-
 while True:
     print("\n--- APPLE TECH STORE ---")
     print("1. View Inventory")
-    print("2. Buy Product")
+    print("2. Buy Products")
     print("3. Exit")
-    
-    choise = input("Choose an option (1-3): ")
-    
+
+    choise = input("Choose an option (1-3):")
+
     if choise == "1":
         print("\nOur current stock and prices:")
-        for item, price in products.items():
-            print(f"{item.replace('_', ' ').title()}: {price} GEL")
-            
+        for item, info in products.items():
+            name = item.replace('_', ' ').title()
+            print(f"{name} - price: {info['price']} GEL | stock: {info['quantity']} pcs")
+    
     elif choise == "2":
         user_input = input("Enter product name: ").replace(" ", "_").lower()
-        
+
         if user_input in products:
-            base_price = products[user_input]
-            
-            if base_price > 1000:
-                final_price = apply_discount(base_price, 100)  
-                print("You got a 10% loyalty discount for premium product!")
+            if products[user_input]["quantity"] > 0:
+                base_price = products[user_input]["price"]
+
+                if base_price > 1000:
+                    final_price = apple_discount(base_price, 10)
+                    print("You got a 10% loyalty discount for premium product!")
+                else: 
+                    final_price = base_price
+                tax = calculate_tax(final_price)
+
+                print(f"Base price: {base_price} GEL")
+                print(f"Final price (with discount): {final_price} GEL")
+                print(f"Tax included (18% VAT): {tax} GEL")
+
+                products[user_input]["quantity"] -= 1
+
+                with open ("my_backup.json", "w") as file:
+                    json.dump(products, file, indent=4)
+
+                print("Order processed successfully! Stock updated.")
             else:
-                final_price = base_price
-                
-            tax = calculate_tax(final_price)
-            
-            print(f"Base price: {base_price} GEL")
-            print(f"Final price (with discount): {final_price} GEL")
-            print(f"Tax included (18% VAT): {tax} GEL")
-            print("Order processed successfully!")
+                print("Sorry, this product is out of stock!")
         else:
             print(f"Sorry, we don't have {user_input} in stock.")
-            
-    elif choise == "3":
+    elif choise == 3:
         print("Thank you for using Apple tech store! Goodbye.")
         break
     else:
