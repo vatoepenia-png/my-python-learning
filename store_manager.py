ecb0e52ae2bca9ede2cd509898460020d5be0569
Vato Ephenia <vatoepenia@gmail.com>
2026-05-19 23:24:34 +0400

    prices, stocks and admin access

--- store_manager.py
+++ store_manager.py
@@ -10,9 +10,10 @@ while True:
     print("\n--- APPLE TECH STORE ---")
     print("1. View Inventory")
     print("2. Buy Products")
-    print("3. Exit")
+    print("3. Admin panel")
+    print("4. Exit")
 
-    choise = input("Choose an option (1-3):")
+    choise = input("Choose an option (1-4):")
 
     if choise == "1":
         print("\nOur current stock and prices:")
@@ -48,9 +49,42 @@ while True:
                 print("Sorry, this product is out of stock!")
         else:
             print(f"Sorry, we don't have {user_input} in stock.")
-    elif choise == 3:
+
+    elif choise == "3":
+        password = input("Enter admin password: ")
+
+        if password == "1234":
+            print("\n--- Welcome to Admin Panel ---")
+            while True:
+                print("\nAdmin Options:")
+                print("1. Restock Product")
+                print("2. Back to Main Menu")
+
+                admin_choise = input("Chose admin option (1-2): ")
+
+                if admin_choise == "1":
+                    prod_name = input("Enter product name to restock (e. g. iphone_15): ").replace(" ", "_").lower()
+
+                    if prod_name in products:
+                        amount = int(input("How mane pieces are you adding to stock?:"))
+                        products[prod_name]["quantity"] += amount
+                        with open("my_backup.json", "w") as file:
+                            json.dump(products, file, indent=4)
+
+                        print(f"Successfully added {amount} pcs to {prod_name.replace('_', ' ').title()}!")
+                    else:
+                        print("This product doesn't exist in our base!")
+                elif admin_choise == "2":
+                    print("Exist Admin  Panel...")
+                    break
+                else:
+                    print("Invalid admin option!")
+        else:
+            print("Acces Denied! Incorrect Password.")
+       
+    elif choise == "4":
         print("Thank you for using Apple tech store! Goodbye.")
         break
     else:
-        print("Invalid option! Please choose 1, 2 or 3.")
+        print("Invalid option! Please choose 1, 2, 3 or 4.")
 
